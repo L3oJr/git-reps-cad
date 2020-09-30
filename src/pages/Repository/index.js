81f7f31f37cfd87de1809dfc49aefd1f79b1c70b
Leonardo Santana Jr <leojuniortj@gmail.com>
2020-09-30 10:48:07 -0300

    Repository page converted using React Hooks

--- src/pages/Repository/index.js
+++ src/pages/Repository/index.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useParams } from 'react-router-dom';
 import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
 import PropTypes from 'prop-types';
 import api from '../../services/api';
@@ -15,78 +15,61 @@ import {
   Controls,
 } from './styles';
 
-export default class Repository extends Component {
-  static propTypes = {
-    match: PropTypes.shape({
-      params: PropTypes.shape({
-        repository: PropTypes.string,
-      }),
-    }).isRequired,
-  };
+export default function Repository() {
+  const [repository, setRepository] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [state, setState] = useState('all');
+  const [page, setPage] = useState(1);
+  const { repo } = useParams();
 
-  state = {
-    repository: {},
-    issues: [],
-    loading: true,
-    state: '',
-    page: 1,
-  };
+  const repoName = decodeURIComponent(repo);
 
-  async componentDidMount() {
-    const { match } = this.props;
-
-    const repoName = decodeURIComponent(match.params.repository);
-
+  useEffect(() => {
     /*
      * Se é necessário aguardar uma requisição após a outra
      * const response = await api.get(`repos/${repoName}`);
      * const issues = await api.get(`repos/${repoName}/issues`);
      */
-
     // Fazendo as chamadas juntas, sem uma aguardar a outra terminar
-    const [repository, issues] = await Promise.all([
-      api.get(`repos/${repoName}`),
-      api.get(`repos/${repoName}/issues`, {
-        params: {
-          per_page: 5,
-        },
-      }),
-    ]);
+    async function updateRepos() {
+      const [reposUpdated, issuesUpdated] = await Promise.all([
+        api.get(`repos/${repoName}`),
+        api.get(`repos/${repoName}/issues`, {
+          params: {
+            per_page: 5,
+          },
+        }),
+      ]);
 
-    this.setState({
-      repository: repository.data,
-      issues: issues.data,
-      loading: false,
-    });
-  }
+      setRepository(reposUpdated.data);
+      setIssues(issuesUpdated.data);
+      setLoading(false);
+    }
+    updateRepos();
+  }, [repoName]);
 
-  handleStateChange = async (e) => {
+  async function handleStateChange(e) {
     try {
       const stateSelected = e.target.value;
       e.preventDefault();
-      const { match } = this.props;
-      const repoName = decodeURIComponent(match.params.repository);
-      const issues = await api.get(`repos/${repoName}/issues`, {
+      const newIssues = await api.get(`repos/${repoName}/issues`, {
         params: {
           state: stateSelected,
           per_page: 5,
           page: 1,
         },
       });
-      this.setState({
-        issues: issues.data,
-        state: stateSelected,
-        page: 1,
-      });
+      setIssues(newIssues.data);
+      setState(stateSelected);
+      setPage(1);
     } catch (err) {
       throw Error(err.message);
     }
-  };
+  }
 
-  handlePageChange = async (direction, e) => {
+  async function handlePageChange(direction, e) {
     e.preventDefault();
-    const { match } = this.props;
-    const { page, state } = this.state;
     let nextPage;
 
     if (direction === 'forward') {
@@ -95,88 +78,86 @@ export default class Repository extends Component {
       nextPage = page - 1;
     }
 
-    const repoName = decodeURIComponent(match.params.repository);
-    const issues = await api.get(`repos/${repoName}/issues`, {
+    const newIssues = await api.get(`repos/${repoName}/issues`, {
       params: {
         state,
         per_page: 5,
         page: nextPage,
       },
     });
-    this.setState({
-      issues: issues.data,
-      page: nextPage,
-      state,
-    });
-  };
-
-  render() {
-    const { repository, issues, loading, page } = this.state;
-
-    if (loading) {
-      return <Loading>Carregando</Loading>;
-    }
-
-    return (
-      <Container>
-        <Owner>
-          <Link to="/">Voltar aos repositórios</Link>
-          <img src={repository.owner.avatar_url} alt={repository.owner.login} />
-          <h1>{repository.name}</h1>
-          <p>{repository.description}</p>
-        </Owner>
-        <Controls>
-          <IssueState>
-            <label htmlFor="issue-state">
-              Filtro:
-              <select id="issue-state" onChange={this.handleStateChange}>
-                <option value="all">Todos</option>
-                <option value="open">Abertos</option>
-                <option value="closed">Fechados</option>
-              </select>
-            </label>
-          </IssueState>
-
-          <Pagination>
-            <button
-              type="button"
-              onClick={(e) => this.handlePageChange('backward', e)}
-              disabled={!(page > 1)}
-            >
-              <FaChevronLeft />
-            </button>
-            <button
-              type="button"
-              onClick={(e) => this.handlePageChange('forward', e)}
-            >
-              <FaChevronRight />
-            </button>
-          </Pagination>
-        </Controls>
-
-        <IssueList>
-          {issues.map((issue) => (
-            <li key={String(issue.id)}>
-              <img src={issue.user.avatar_url} alt={issue.user.login} />
-              <div>
-                <strong>
-                  <a
-                    href={issue.html_url}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {issue.title}
-                  </a>
-                  {issue.labels.map((label) => (
-                    <span key={String(label.id)}>{label.name}</span>
-                  ))}
-                </strong>
-                <p>{issue.user.login}</p>
-              </div>
-            </li>
-          ))}
-        </IssueList>
-      </Container>
-    );
+    setIssues(newIssues.data);
+    setPage(nextPage);
+    setState(state);
   }
+
+  if (loading) {
+    return <Loading>Carregando</Loading>;
+  }
+
+  return (
+    <Container>
+      <Owner>
+        <Link to="/">Voltar aos repositórios</Link>
+        <img src={repository.owner.avatar_url} alt={repository.owner.login} />
+        <h1>{repository.name}</h1>
+        <p>{repository.description}</p>
+      </Owner>
+      <Controls>
+        <IssueState>
+          <label htmlFor="issue-state">
+            Filtro:
+            <select id="issue-state" onChange={(e) => handleStateChange(e)}>
+              <option value="all">Todos</option>
+              <option value="open">Abertos</option>
+              <option value="closed">Fechados</option>
+            </select>
+          </label>
+        </IssueState>
+
+        <Pagination>
+          <button
+            type="button"
+            onClick={(e) => handlePageChange('backward', e)}
+            disabled={!(page > 1)}
+          >
+            <FaChevronLeft />
+          </button>
+          <button type="button" onClick={(e) => handlePageChange('forward', e)}>
+            <FaChevronRight />
+          </button>
+        </Pagination>
+      </Controls>
+
+      <IssueList>
+        {issues.map((issue) => (
+          <li key={String(issue.id)}>
+            <img src={issue.user.avatar_url} alt={issue.user.login} />
+            <div>
+              <strong>
+                <a
+                  href={issue.html_url}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  {issue.title}
+                </a>
+                {issue.labels.map((label) => (
+                  <span key={String(label.id)}>{label.name}</span>
+                ))}
+              </strong>
+              <p>{issue.user.login}</p>
+            </div>
+          </li>
+        ))}
+      </IssueList>
+    </Container>
+  );
 }
+
+Repository.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      repo: PropTypes.string,
+    }),
+  }).isRequired,
+};
